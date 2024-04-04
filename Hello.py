1bda7b9fe3892ee037a4205322cd9aba49e2bf1d
Ansontnw <165770344+Ansontnw@users.noreply.github.com>
2024-04-04 20:27:47 +0800

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -9,7 +9,7 @@ if connectionType:
     st.write("You selected {} connection.".format(connectionType))
 
 if connectionType == "Series":
-     n = st.number_input("Enter the number of resistors in series:", min_value = 1, step = 1, value = 1)
+     n = st.slider_input("Enter the number of resistors in series:", min_value = 1, step = 1, value = 1)
      resistors = []
      for i in range(n):
             r = st.number_input(f"Enter resistance {i + 1} (in ohms):")
@@ -23,19 +23,17 @@ elif connectionType == "Parallel":
      for i in range(n):
           r = st.number_input(f"Enter resistance {i + 1} (in ohms):")
           resistors.append(r)
-
-     # Check if any resistor value is zero
      if 0 in resistors:
-         st.write("One or more resistor values is zero. Cannot calculate parallel resistance.")
+         st.write("If resistor values is zero. Cannot calculate parallel resistance.")
      else:
          total_resistance = 1 / sum(1 / r for r in resistors)
-         st.write("Total resistance value in parallel is {:.2f} ohms.".format(total_resistance))
+         st.write("Total value resistance in parallel is {:.2f} ohms.".format(total_resistance))
 
 #elif connectionType == "Parallel":
- #    n = st.number_input("Enter the number of resistors in parallel:", min_value = 1, step = 1, value = 1)
-  #   resistors = []
-   #  for i in range(n):
-    #      r = st.number_input(f"Enter resistance {i + 1} (in ohms):")
-     #     resistors.append(r)
-     #total_resistance = 1 / sum(float(1 / r for r in resistors))
-     #st.write("Total value resistance in series is {:.2f} ohms.".format(total_resistance))
+#     n = st.number_input("Enter the number of resistors in parallel:", min_value = 1, step = 1, value = 1)
+#     resistors = []
+#     for i in range(n):
+#          r = st.number_input(f"Enter resistance {i + 1} (in ohms):")
+#          resistors.append(r)
+#     total_resistance = 1 / sum(float(1 / r for r in resistors))
+#     st.write("Total value resistance in series is {:.2f} ohms.".format(total_resistance))
