b710c17197527c9a1d0944f02a54aa383a75b298
Ansontnw <165770344+Ansontnw@users.noreply.github.com>
2024-04-04 20:19:35 +0800

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -18,10 +18,24 @@ if connectionType == "Series":
      st.write("Totol value resistance in series is {:.2f} ohms.".format(total_resistance))
 
 elif connectionType == "Parallel":
-     n = st.number_input("Enter the number of resistors in parallel:", min_value = 1, step = 1, value = 1)
+     n = st.number_input("Enter the number of resistors in parallel:", min_value=1, step=1, value=1)
      resistors = []
      for i in range(n):
           r = st.number_input(f"Enter resistance {i + 1} (in ohms):")
           resistors.append(r)
-     total_resistance = 1 / sum(float(1 / r for r in resistors))
-     st.write("Total value resistance in series is {:.2f} ohms.".format(total_resistance))
+
+     # Check if any resistor value is zero
+     if 0 in resistors:
+         st.write("One or more resistor values is zero. Cannot calculate parallel resistance.")
+     else:
+         total_resistance = 1 / sum(1 / r for r in resistors)
+         st.write("Total resistance value in parallel is {:.2f} ohms.".format(total_resistance))
+
+#elif connectionType == "Parallel":
+ #    n = st.number_input("Enter the number of resistors in parallel:", min_value = 1, step = 1, value = 1)
+  #   resistors = []
+   #  for i in range(n):
+    #      r = st.number_input(f"Enter resistance {i + 1} (in ohms):")
+     #     resistors.append(r)
+     #total_resistance = 1 / sum(float(1 / r for r in resistors))
+     #st.write("Total value resistance in series is {:.2f} ohms.".format(total_resistance))
