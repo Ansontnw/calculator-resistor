9a3150ea872ceb2685cea3a36c5fe9375880f1e3
Ansontnw <165770344+Ansontnw@users.noreply.github.com>
2024-04-04 20:17:19 +0800

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -1,17 +1,3 @@
-# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import streamlit as st
 
 st.title("Hello, Wellcome to my Resistor Calculator👋")
@@ -37,5 +23,5 @@ elif connectionType == "Parallel":
      for i in range(n):
           r = st.number_input(f"Enter resistance {i + 1} (in ohms):")
           resistors.append(r)
-     total_resistance = 1 / sum(1 // r for r in resistors)
+     total_resistance = 1 / sum(float(1 / r for r in resistors))
      st.write("Total value resistance in series is {:.2f} ohms.".format(total_resistance))
